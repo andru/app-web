85b794a2876cf89ed44c1573ab8eabd5c56ec7f0
Andru Vallance <andru@tinymighty.com>
2016-01-01 18:21:28 +0100

    feat(redux) add plants and places

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -3,11 +3,19 @@ import { routeReducer } from 'redux-simple-router'
 
 import counter from './counter'
 import plantings from './plantings'
+import plants from './plants'
+import places from './places'
 import timeline from './timeline'
 
 export default combineReducers({
   counter,
+
+  // app data
   plantings,
+  plants,
+  places,
+
+  // views
   timeline,
   router: routeReducer
 })
