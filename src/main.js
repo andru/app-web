44a0e704bad32dffbf7413a73499e6b90c9fab42
Andru Vallance <andru@tinymighty.com>
2016-02-08 11:01:21 +0100

    feat(POJOs to Maps) convert initialization

--- src/main.js
+++ src/main.js
@@ -6,15 +6,20 @@ import Root from './containers/Root'
 import configureStore from './redux/configureStore'
 import Hoodie from 'hoodie-client'
 
+import modelArrayToMap from 'utils/modelArrayToMap'
+
 const history = createHistory()
 
-const store = configureStore(require('./initialDevState.json'), history)
+// TODO initialize data from Pouch/Hoodie
+const initialState = require('./initialDevState.json')
+initialState.plantings = modelArrayToMap(initialState.plantings)
+initialState.places = modelArrayToMap(initialState.places)
+initialState.plants = modelArrayToMap(initialState.plants)
+
+const store = configureStore(initialState, history)
 
 // Render the React application to the DOM
 render(
   <Root history={history} routes={routes} store={store} />,
   document.getElementById('root')
 )
-
-
-
