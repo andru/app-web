5a2ed30ee599a053740b92093ab11ae96657bb67
Andru Vallance <andru@tinymighty.com>
2016-04-22 12:02:44 +0200

    chore(storybook) add web pack modules for css, less, web fonts

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  plugins: [
+    // your custom plugins
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: [
+          'style',
+          'css?sourceMap',
+          'postcss'
+        ]
+      },
+      {
+        test: /\.less$/,
+        loaders: [
+          'style',
+          'css?sourceMap',
+          'postcss',
+          'less'
+        ]
+      },
+      { test: /\.woff(\?.*)?$/,  loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff' },
+      { test: /\.woff2(\?.*)?$/, loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff2' },
+      { test: /\.ttf(\?.*)?$/,   loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?.*)?$/,   loader: 'file?prefix=fonts/&name=[path][name].[ext]' },
+      { test: /\.svg(\?.*)?$/,   loader: 'file?prefix=svg/&name=[path][name].[ext]' },
+      { test: /\.(png|jpg|gif)$/,    loader: 'url?limit=8192' }
+    ]
+  },
+  resolve: {
+    fallback: [
+      path.resolve('./src')
+    ],
+    extensions: ['', '.js', '.jsx']
+  }
+}
